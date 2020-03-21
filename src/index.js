4b1eea911b74b6c36901339ba6c8f4183bff1c7e
Igorekmog <m19_bond@tut.by>
2020-03-21 14:11:02 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if(str.includes(bracketsConfig[i].join('')) ) {
+      str = str.replace(bracketsConfig[i].join(''), '');
+      i = -1;
+    } 
+  }
+  console.log(str);
+  if(str=='')
+  return true;
+  else return false;
 }
